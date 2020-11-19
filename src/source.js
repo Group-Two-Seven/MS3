54ca87e580ed95cecbccfb06df26aab0b8816176
Luke <lukebmorrow@gmail.com>
2020-11-18 18:07:19 -0600

    Added tab switching and made some minor visual improvements.

--- src/source.js
+++ src/source.js
@@ -3,7 +3,31 @@
 
     rows.forEach(row => {
         row.addEventListener("click", () => {
-            alert('Tab \'' +row.dataset.tabname+'\' clicked!');
+            changeTab(row.dataset.tabname)
         })
     }) 
 })
+
+function changeTab(tab_name){
+    const rows = $(".tab");
+    const screens = $("#main_content").children()
+    console.log(screens)
+
+    for(var i=0; i<rows.length; i++){
+        if(rows[i].getAttribute("data-tabname") == tab_name){
+            rows[i].classList.add("active");
+
+        }else{
+            rows[i].classList.remove("active");
+        }
+    }
+
+    for(var i=0; i<screens.length; i++){
+        if(screens[i].id == tab_name){
+            screens[i].removeAttribute("hidden")
+        }else{
+            screens[i].setAttribute("hidden","true")
+        }
+    }
+    return true;
+}
