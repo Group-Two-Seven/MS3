e111fdfa32ef88de9dc433ac7d51aafb009e1997
Luke <lukebmorrow@gmail.com>
2020-11-18 18:14:08 -0600

    Added comments and removed a console.log call.

--- src/source.js
+++ src/source.js
@@ -2,17 +2,18 @@
     const rows = document.querySelectorAll("tr[data-tabname]");
 
     rows.forEach(row => {
-        row.addEventListener("click", () => {
+        row.addEventListener("click", () => {// This straps a hidden button onto each tab that will call the "changeTab" function
             changeTab(row.dataset.tabname)
         })
     }) 
 })
 
 function changeTab(tab_name){
+    //get all elements needed
     const rows = $(".tab");
     const screens = $("#main_content").children()
-    console.log(screens)
 
+    //Remove the highlight from the previous tab and highlight the new tab
     for(var i=0; i<rows.length; i++){
         if(rows[i].getAttribute("data-tabname") == tab_name){
             rows[i].classList.add("active");
@@ -22,9 +23,10 @@ function changeTab(tab_name){
         }
     }
 
+    //Hide old screen content and unhide new screen content
     for(var i=0; i<screens.length; i++){
         if(screens[i].id == tab_name){
-            screens[i].removeAttribute("hidden")
+            screens[i].removeAttribute("hidden")//Here is the workaround for setAttribute("hidden","false"), which wasn't working.
         }else{
             screens[i].setAttribute("hidden","true")
         }
