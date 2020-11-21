af022a136de7976c08a254e223abf2b1ae955b56
Luke <lukebmorrow@gmail.com>
2020-11-20 22:26:20 -0600

    Attached basic modals to menu

--- src/source.js
+++ src/source.js
@@ -1,47 +1,55 @@
  var menu_items= [
      {
+         id:1,
          name: "Pepperoni Pizza",
          description:"description 1",
          image_path:"img/dishes/dish1.png",
          image_alt:"Pepperoni Pizza"
      },
      {
+        id:2,
         name: "Cheese Pizza",
         description:"description 2",
         image_path:"img/dishes/dish2.png",
         image_alt:"Cheese Pizza"
     },
     {
+        id:3,
         name: "Canadian Pizza",
         description:"description 3",
         image_path:"img/dishes/dish3.png",
         image_alt:"Canadian Pizza"
     },
     {
+        id:4,
         name: "Beef Noodlefull Bowl",
         description:"description 4",
         image_path:"img/dishes/dish4.png",
         image_alt:"Beef Noodlefull Bowl"
     },
     {
+        id:5,
         name: "Veggie Noodlefull Bowl",
         description:"description 5",
         image_path:"img/dishes/dish5.png",
         image_alt:"Veggie Noodlefull Bowl"
     },
     {
+        id:6,
         name: "Kingburger Supreme",
         description:"description 6",
         image_path:"img/dishes/dish6.png",
         image_alt:"Kingburger Supreme"
     },
     {
+        id:7,
         name: "Waffle Bowl",
         description:"description 7",
         image_path:"img/dishes/dish7.png",
         image_alt:"Waffle Bowl"
     },
     {
+        id:8,
         name: "White Chocolate Waffle Bowl",
         description:"description 8",
         image_path:"img/dishes/dish8.png",
@@ -78,7 +86,7 @@
                 <div>'+menu_items[i].description+'</div>\
             </div>\
         </div>')
-        attachModal(menu_items[i])
+        attachModal(menu_items[i], i)
     }
 
     for(var i=0; i<4; i++){// 4 is the number of popular items that can fit in the popular items section
@@ -89,8 +97,10 @@
                     <div><strong>'+menu_items[i].name+'</strong></div>\
                 <div>'+menu_items[i].description+'</div>\
             </div>\
-        </div>')
-        attachModal(menu_items[i])
+        </div>');
+
+        attachModalHighlight(menu_items[i].id, i)
+
     }
 })
 
@@ -126,28 +136,47 @@ function openMenu(menu_name){
 
 }
 
-function attachModal(item){
-
-    $("#menu_item_popup").append('<div class="modal-dialog">\
-        <div class="modal-content">\
-            <div class="modal-body">\
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">\
-                <span aria-hidden="true">&times;</span>\
-            </button>\
-            <div class="rows">\
-                <div class="col-6" id="modal_left_col">\
-                <div id="modal_image"><img src="'+item.image_path+'" alt="'+item.image_alt+'"/></div>\
-                <div id="modal_description">'+item.deep_description+'</div>\
+function attachModal(item, id){
+    $("#modal_placeholder").append('\
+    <div class="modal" tabindex="-1" id="menu_item_'+item.id+'_popup">\
+        <div class="modal-dialog">\
+            <div class="modal-content">\
+                <div class="modal-body">\
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">\
+                    <span aria-hidden="true">&times;</span>\
+                </button>\
+                <div class="rows">\
+                    <div class="col-6" id="modal_left_col">\
+                    <div id="modal_image"><img src="'+item.image_path+'" alt="'+item.image_alt+'"/></div>\
+                    <div id="modal_description">'+item.deep_description+'</div>\
+                    </div>\
+                    <div class="col-6" id="modal_right_col">\
+                    <div><input type="number"></div>\
+                    <div id="modal_options">'+item.options+'</div>\
+                    </div>\
                 </div>\
-                <div class="col-6" id="modal_right_col">\
-                <div><input type="number"></div>\
-                <div id="modal_options">'+item.options+'</div>\
                 </div>\
-            </div>\
-            </div>\
-            <div class="modal-footer">\
-            <button type="button" class="btn">Add to Cart</button>\
+                <div class="modal-footer">\
+                <button type="button" class="btn">Add to Cart</button>\
+                </div>\
             </div>\
         </div>\
     </div>')
+
+    document.getElementById("menu_item_"+id).addEventListener("click", ()=>{
+        $("#menu_item_"+item.id+"_popup").modal("show");
+    })
+}
+
+function attachModalHighlight(menu_item_id, id){
+
+
+        document.getElementById("menu_item_highlight_"+id).addEventListener("click", ()=>{
+            $("#menu_item_"+menu_item_id+"_popup").modal("show");
+        })
+
+        
+  
+    
+    
 }
